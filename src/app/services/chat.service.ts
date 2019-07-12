0a2100c97c893f66803472cfb68b41968e7dba7b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2019-07-12 23:45:25 +0530

    Auto-refresh list of channels

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, timer } from 'rxjs';
 import environment from 'src/environments/environment';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, concatMap } from 'rxjs/operators';
 import { Channel } from '../dto';
 
 @Injectable({
@@ -10,10 +10,11 @@ import { Channel } from '../dto';
 })
 export class ChatService {
 
-  constructor(private httpCLient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
   getChannels(): Observable<Channel> {
-    return this.httpCLient.get(`${environment.backendUrl}/channels`)
-      .pipe(map((response: any) => response._embedded.channels), tap(console.log))
+    return timer(0, 10000)
+      .pipe(concatMap(() => this.httpClient.get(`${environment.backendUrl}/channels`)
+        .pipe(map((response: any) => response._embedded.channels), tap(console.log))));
   }
 }
